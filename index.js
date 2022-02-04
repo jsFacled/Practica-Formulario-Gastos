c1dd2c68e6987e5ba33d13e2305a7b3b126c13b3
jsFacled <facled@gmail.com>
2022-02-04 13:46:23 -0300

    agregando javascript al botón Añadir addEventLis y preventDefault

---
+++ index.js
@@ -0,0 +1,10 @@
+//Capto el formulario
+const form = document.getElementById("form");
+
+//Escucho el evento al clickear Añadir y le instancio una función:
+form.addEventListener("submit", function (event) {
+  /*luego de captar el event le indicamos que no recargue la página con preventDefault.*/
+  event.preventDefault();
+  /* Notar que el submit ocurrió de todos modos. Pero no se envía al servidor. Lo corroboramos con console log. Luego enviaremos al servidor de otra forma, por ejemplo con ajax.*/
+  console.log(event);
+});
