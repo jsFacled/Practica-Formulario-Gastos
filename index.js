dd0074d8ad64148dfaa2ff2356362e5ab48e3ea7
jsFacled <facled@gmail.com>
2022-02-05 00:32:08 -0300

    Agregando datos del formulario a la Tabla con insertRow insertCell

--- index.js
+++ index.js
@@ -7,8 +7,15 @@ form.addEventListener("submit", function (event) {
   event.preventDefault();
   /* Notar que el submit ocurrió de todos modos. Pero no se envía al servidor. Lo corroboramos con console log. Luego enviaremos al servidor de otra forma, por ejemplo con ajax.*/
   let formData = new FormData(form);
+  let tablaRef = document.getElementById("tabla1");
+  let newTransactionRowRef = tablaRef.insertRow(-1);
+
+  let newTypeCellRef = newTransactionRowRef.insertCell(0);
+  newTypeCellRef.textContent = formData.get("type");
+  newTypeCellRef = newTransactionRowRef.insertCell(1);
+  newTypeCellRef.textContent = formData.get("descripcion");
+  newTypeCellRef = newTransactionRowRef.insertCell(2);
+  newTypeCellRef.textContent = formData.get("monto");
+  newTypeCellRef = newTransactionRowRef.insertCell(3);
+  newTypeCellRef.textContent = formData.get("cat");
 });
-
-
-
-
